cb91114db8070f28cff87605516ef6d281469f5a
carlestecno <13carlestecno@gmail.com>
2022-09-25 20:53:32 +0200

    new changes

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -4,29 +4,15 @@
 
 int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-    size_t  i;
-
-    i = 0;
-    while (i <= n)
+    while (n--)
     {
-        if (s1[i] != s2[i])
+        if (*s1 != *s2)
         {
-            if (s1[i] < s2[i])
+            if (*s1 < *s2)
                 return (-1);
             else
                 return (1);
         }
-        i++;
     }
-    return (s1[i] - s2[i]);
-}
-
-
-int main()
-{
-    char    *str;
-
-    printf("el valor es: %d\n", strncmp("zatatata", "bbtata", 1* sizeof(char)));
-    printf("el valor es: %d", ft_strncmp("zatatata", "bbtata", 1 * sizeof(char)));
-    return (0);
+    return (*s1 - *s2);
 }
