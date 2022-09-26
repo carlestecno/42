95f7b30861f87294982136b63537ca62d9c6dd83
carlestecno <13carlestecno@gmail.com>
2022-09-26 09:46:09 +0200

    void    *ft_memchr(const void *s, int c, size_t n) { 	char *ptr;
    	ptr = s;
        while (n--)
    	{
    		if (*ptr == c)
    		{
    			return (ptr);
    		}
    		ptr++;
    	}
    	return (0);
    
    }
    ultim commit

--- ft_memchr.c
+++ ft_memchr.c
@@ -4,25 +4,16 @@
 
 void    *ft_memchr(const void *s, int c, size_t n)
 {
-    size_t  i;
-    char    *str;
-
-    str = (char*)s;
-    i = 0;
-    while (str[i] && (i <= n))
-    {
-        if (str[i] == c )
-        {
-            return (s);
-        }
-        i++;
-    }
-    return (0); 
-}
-
-int main(void)
-{
-    /* printf("El valor de: %s", ft_memchr("Patata", 't', 2 * sizeof(char))); */
-    printf("El valor de: %s", memchr("Patata", 't', 2 * sizeof(char)));
-    return (0);
+	char *ptr;
+	
+	ptr = s;
+    while (n--)
+	{
+		if (*ptr == c)
+		{
+			return (ptr);
+		}
+		ptr++;
+	}
+	return (0);
 }
