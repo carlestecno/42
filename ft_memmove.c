1c79763d1fa1eb6d8a7a1918aecc2db0ac42b672
carlestecno <13carlestecno@gmail.com>
2022-09-27 14:19:46 +0200

    a veur

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,28 +6,13 @@ void    *ft_memmove(void *restrict dest, const void *restrict src, size_t n)
 {
     char *str_dest;
     char *str_source;
-    char *str_newdest;
-    char  temp;
 
-    str_dest = (char *)dest;
-    str_source = (char *)src;
-    str_newdest = str_dest + (str_dest - str_source);
-    printf("la cadena de newdest: %c\n", *str_newdest);
-    printf("diferencia de (dest - source):%d\n", str_dest - str_source);
-    while (n--)
+
+    str_dest = dest;
+    str_source = src;
+    while (dest-src)
     {
-      printf("str_dest abans: %c\n", *str_dest);
-      *str_dest = *str_source;
-      str_dest++;
-      str_source++;
-      *str_newdest = *str_dest;
-      printf("str_dest despres: %c\n", *str_dest);
-      temp  = *str_dest++;
-      printf("valor del temp:%c\n", temp);
-      *str_newdest++ = *str_dest++;
-      *(str_dest)++ = temp;
-      
-      printf("valor del str_newdest:%c\n", *str_newdest);
+      *str_dest++ = *str_source++;
     }
     return (str_dest);
 }
@@ -36,8 +21,6 @@ void    *ft_memmove(void *restrict dest, const void *restrict src, size_t n)
 int main()
 {
     char    str[] = "0123456789abcdfghikl";
-    // char    dest[50]  ="Patata de Patata";
-    
-    ft_memmove(str + 5 , str  , 6* sizeof(char));
-    printf("Lstring0 es de: %s\n", str);
+    char    str2[] = "0123456789abcdfghikl";
+    printf("El valor es de: %s", ft_memmove(str + 5 , str  , 6* sizeof(char)));
 }
--- libft.h
+++ libft.h
@@ -1,9 +1,14 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <string.h>
+
 int     ft_isalpha(int c);
 int     ft_isdigit(int c);
 int     ft_isalnum(int c);
 int     ft_isascii(int c);
 int     ft_isprint(int c);
-int     ft_strlen(char *str);
+size_t     ft_strlen(char *str);
 int     ft_toupper(int c);
 int     ft_tolower(int c);
 int     ft_strncmp(const char *s1, const char *s2, size_t n);
@@ -18,6 +23,8 @@ size_t  strlcpy(char *dst, const char *src, size_t size);
 
 char    *ft_strchr(const char *s, int c);
 char    *ft_strrchr(const char *s, int c);
+
+#endif
 
 
 
