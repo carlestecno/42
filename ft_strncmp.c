fb582781f7a6632563ec06f8005dd176608eaace
carlestecno <13carlestecno@gmail.com>
2022-10-25 20:03:21 +0200

    news

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -4,11 +4,16 @@
 
 int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-    while (n-- && *s1 && *s2)
+    unsigned char    a;
+    unsigned char    b;
+
+    while (n-- && (*s1 || *s2))
     {
-        if (*s1 != *s2)
+        a = (unsigned char)*s1;
+        b = (unsigned char)*s2;
+        if (a != b)
         {
-            return ((unsigned char *)s1 - (unsigned char *)s2);      
+            return (a - b);      
         }
         s1++;
         s2++;
